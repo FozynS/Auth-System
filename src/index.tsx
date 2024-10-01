70b71c988569a601654c234c489ee92b603100a4
Danylo Shvydkyi <wolf44488@gmail.com>
2024-10-01 15:55:41 +0200

    update: add more styles

--- src/index.tsx
+++ src/index.tsx
@@ -3,7 +3,7 @@ import App from './App';
 import { Provider } from 'react-redux';
 import store from './redux/store';
 import { GoogleOAuthProvider } from '@react-oauth/google';
-// import './styles/global.scss';
+import './styles/global.scss';
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 root.render(
